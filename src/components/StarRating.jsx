cb7e4cde7b7046242bb8ec1025369d154715891e
major <majorwin541@gmail.com>
2023-07-14 08:39:22 +0000

    creating the starts work

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Star from "./Star";
 
 const StarRating = ({ maxRating = 5 }) => {
   // {maxRating} =5 , means that while destracturing it gives maxrating a default value if not given first when called
+  const [rating, setRating] = useState(0);
   return (
     <div className='border  flex align-middle gap-4'>
       <div className=' flex gap-3'>
         {Array.from({ length: maxRating }, (_, i) => (
-          <span>S{i + 1}</span>
+          //   <span>S{i + 1}</span>
+          <Star
+            key={i}
+            full={rating >= i + 1}
+            onRate={() => setRating(i + 1)}
+          />
         ))}
       </div>
-      <p>{maxRating}</p>
+      <p>{rating || ""}</p>
     </div>
   );
 };
