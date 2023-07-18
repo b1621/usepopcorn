4e1cd5ba4dfde1d48ec66067f092b2e79e4da4d8
major <majorwin541@gmail.com>
2023-07-18 17:01:09 +0300

    working on loading movie detail

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BsArrowLeft } from "react-icons/bs";
 
 const MovieDetails = ({ selectedId, onCloseMovie }) => {
+  const [movie, setMovie] = useState({});
+  useEffect(() => {
+    const getMovieDetail = async () => {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=44c8fa82&i=${selectedId}`
+      );
+      const data = await res.json();
+      setMovie(data);
+    };
+
+    getMovieDetail();
+  }, []);
   return (
     <div>
       <div>
